4d963ceb086f3d38ec5b1b278eb0e878da93edb5
VHarmony <harmony.dgt@gmail.com>
2025-08-14 10:26:33 -0300

    repoVite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/...
 export default defineConfig(({ mode }) => ({
-    base: '/https://github.com/andersonferreiradecastro-boop/anderson-moveis-porto-alegre/',
+    base: '/anderson-moveis-porto-alegre/',
     build: {
     outDir: 'docs',    // <- Build vai direto para 'docs'
   },
